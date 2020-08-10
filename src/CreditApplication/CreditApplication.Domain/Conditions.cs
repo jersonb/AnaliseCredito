5385355899aa77428a7f40569e23344153ee1976
jersonb <jersonlbrito@hotmail.com>
2020-08-10 09:08:48 -0300

    adicionado strategy

---
+++ src/CreditApplication/CreditApplication.Domain/Conditions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditApplication.Domain
+{
+    public class Conditions
+    {
+        public decimal RequestedAmount { get; set; }
+        public int Portion { get; set; }
+        public DateTime FirstPayment { get; set; }
+        public CreditType CreditType { get; set; }
+    }
+}
---
+++ src/CreditApplication/CreditApplication.Domain/Credit.cs
@@ -0,0 +1,23 @@
+﻿using CreditApplication.Domain.Credits;
+using CreditApplication.Domain.Property;
+
+namespace CreditApplication.Domain
+{
+    public static class Credit
+    {
+        public static ICredit GetCredit(this Conditions conditions)
+        {
+            var proposal = Proposal.GetProposal(conditions.RequestedAmount, conditions.Portion, conditions.FirstPayment);
+
+            return conditions.CreditType switch
+            {
+                CreditType.Direct => CreditBase.GetInstance<Direct>(proposal),
+                CreditType.Business => CreditBase.GetInstance<Business>(proposal),
+                CreditType.Payroll => CreditBase.GetInstance<Payroll>(proposal),
+                CreditType.Personal => CreditBase.GetInstance<Personal>(proposal),
+                CreditType.RealEstate => CreditBase.GetInstance<RealEstate>(proposal),
+                _ => null
+            };
+        }
+    }
+}
