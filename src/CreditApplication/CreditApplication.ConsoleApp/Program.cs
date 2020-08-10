58e9c64058a467593ef2c334e64f6d436fbc0fac
jersonb <jersonlbrito@hotmail.com>
2020-08-10 09:09:29 -0300

    adicionando aplicação console

---
+++ src/CreditApplication/CreditApplication.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using CreditApplication.Domain;
+using System;
+
+namespace CreditApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            var conditions = new Conditions();
+
+            var credit = conditions.GetCredit();
+
+            Console.WriteLine($"{credit.Amount} , {credit.Interest}, {credit.IsAproved}");
+        }
+    }
+}
